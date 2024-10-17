00eee8894a2375d8f1ff203efd72499169360b8b
luckysingh45 <162437679+luckysingh45@users.noreply.github.com>
2024-10-17 14:16:08 +0000

    container config done

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,12 @@
+"use strict"//treat all js code as  newer  version
+//alert(3+3)//we are using node js not browser
+//number=> 2 to 53
+//bigint
+//null=>standalone value
+//undefined
+//symbol=>uniqueness
+
+let name="lucky"
+//object
+console.log(typeof "lucky");
+
